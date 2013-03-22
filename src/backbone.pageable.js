5ae5bcb2aa79400ca5e95d2a099c69bd13c43b3c
Eric <git@heme.net>
2013-03-22 12:24:09 -0500

    Added Defaults and Initialization
    The initialize method binds "this" to get scoping to work correctly within the pagination object. We only need that in order to set pagination.collection to the parent collection. I'm still not sure this pattern is o.k. to use (I'd love feedback!). Also, since it was somewhat likely that the initialize method would be overwritten when this is actually used, I thought it better to make the initialize work in a separate function that can be called manually. Binding "this" seemed better than making the developer know to set "pagination.collection = this" in the initialize method they write when using Pageable.

--- src/backbone.pageable.js
+++ src/backbone.pageable.js
@@ -1,7 +1,51 @@
 Backbone.Pageable = (function(Backbone, _) {
     "use strict";
     
+    var Pageble = {}
     Pageable = Backbone.Collection.extend({
+
+        // Defaults
+        pagination: {
+            collection: null,
+            request: {
+                timeout: 25000,
+                cache: false,
+                type: 'GET',
+                dataType: 'json'
+            },
+            state: {
+                firstPage: 1,
+                currentPage: 1,
+                perPage: 10,
+                totalPages: 10,
+                totalRecords:0,
+                startRecord:function(){ return (this.currentPage - 1) * this.perPage},
+                endRecord: function(){ return this.currentPage * this.perPage -1}
+            },
+            querystring: {},
+            initialize: function(){
+                this.pagination.setDefaults.call(this);
+                this.pagination.collection = this;
+            },
+            setDefaults: function(){
+                _.extend(this.pagination.request, this.pagination_request);
+                _.extend(this.pagination.state, this.pagination_state);
+                _.extend(this.pagination.querystring, this.pagination_querystring);
+            },
+            fetch: function(){
+                var fetchCollection = new Backbone.Collection();
+                return fetchCollection.add(this.collection.slice(this.state.startRecord(), this.state.endRecord()));
+            },
+            toJSON: function(){
+                return this.collection.slice(begin, end);
+            }
+        },
+
+        // If Pageable is extended and this initialize is overwritten then 
+        // you will need to make this same call in your initialize function
+        initialize: function(){
+            this.pagination.initialize.call(this);
+        }
 
     });
     
